cb52f21386253628787b38f73be7f060dc917a13
clf94-dev <clf94@hotmail.es>
2020-10-20 23:05:20 +0200

    added home stories data

--- src/app/components/stories-home/stories-home.component.ts
+++ src/app/components/stories-home/stories-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StoriesData } from './storiesData';
 
 @Component({
   selector: 'app-stories-home',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoriesHomeComponent implements OnInit {
 arrow="../../../assets/images/shared/desktop/arrow.svg"
-  constructor() { }
+
+public storiesdata: StoriesData[];
+  constructor() { 
+    this.storiesdata=[
+      new StoriesData("","The Mountains","by John Appleseed","../../../assets/images/stories/desktop/mountains.jpg","The Mountains"),
+      new StoriesData("","Sunset Cityscapes","by Benjamin Cruz","../../../assets/images/stories/desktop/cityscapes.jpg","Sunset Cityscapes"),
+      new StoriesData("","18 Days Voyage","by Alexei Borodin","../../../assets/images/stories/desktop/18-days-voyage.jpg","18 Days Voyage"),
+      new StoriesData("","Architecturals","by Samantha Brooke","../../../assets/images/stories/desktop/architecturals.jpg","Architecturals")
+    ];
+  }
 
   ngOnInit(): void {
   }
---
+++ src/app/components/stories-home/storiesData.ts
@@ -0,0 +1,13 @@
+export class StoriesData{
+    constructor(
+public date: string,
+public title: string,
+
+public author: string,
+public src: string,
+public imgAlt: string
+
+    ){
+
+    }
+}
