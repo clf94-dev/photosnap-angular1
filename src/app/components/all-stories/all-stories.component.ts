e2cb9bd29ae175b391daf61d512aa03771066eec
clf94-dev <clf94@hotmail.es>
2020-10-21 10:39:24 +0200

    added allStories struct component

--- src/app/components/all-stories/all-stories.component.ts
+++ src/app/components/all-stories/all-stories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StoriesData } from '../stories-home/storiesData';
 
 @Component({
   selector: 'app-all-stories',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-stories.component.scss']
 })
 export class AllStoriesComponent implements OnInit {
-
+  public storiesdata: StoriesData[];
   constructor() { }
 
   ngOnInit(): void {
