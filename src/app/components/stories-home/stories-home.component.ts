6b922110bacf3699b53c097ff6c34ab475d819fb
clf94-dev <clf94@hotmail.es>
2020-10-28 14:42:59 +0100

    added link to btn storiesHome component

--- src/app/components/stories-home/stories-home.component.ts
+++ src/app/components/stories-home/stories-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { StoriesData } from './storiesData';
 
 @Component({
@@ -10,7 +11,7 @@ export class StoriesHomeComponent implements OnInit {
 arrowW="../../../assets/images/shared/desktop/arrow copy.svg"
 
 public storiesdata: StoriesData[];
-  constructor() { 
+  constructor(private _route: ActivatedRoute, private _router: Router) { 
     this.storiesdata=[
       new StoriesData("","The Mountains","by John Appleseed","../../../assets/images/stories/desktop/mountains.jpg","The Mountains"),
       new StoriesData("","Sunset Cityscapes","by Benjamin Cruz","../../../assets/images/stories/desktop/cityscapes.jpg","Sunset Cityscapes"),
@@ -21,5 +22,9 @@ public storiesdata: StoriesData[];
 
   ngOnInit(): void {
   }
+  goToStories(){
+    this._router.navigate(['/stories']);
+  }
+
 
 }
